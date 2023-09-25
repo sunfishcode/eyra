c46cd1d47719e69bc9fc8d3a605340381f9869a2
Dan Gohman <dev@sunfishcode.online>
2023-09-25 16:53:22 -0700

    Import Mustang's tests and examples. (#1)
    We may want to investigate some way for Mustang and Eyra to share tests,
    but for now, this at least gives Eyra a decent amount of tests to run in CI.

---
+++ examples/test-ctor.rs
@@ -0,0 +1,93 @@
+extern crate eyra;
+
+use std::ffi::{CStr, OsStr, OsString};
+use std::os::raw::{c_char, c_int};
+use std::os::unix::ffi::OsStrExt;
+use std::slice;
+use std::sync::atomic::{AtomicBool, Ordering};
+
+static EARLY_CTOR_INITIALIZED: AtomicBool = AtomicBool::new(false);
+static CTOR_INITIALIZED: AtomicBool = AtomicBool::new(false);
+static MANUAL_CTOR_INITIALIZED: AtomicBool = AtomicBool::new(false);
+static DTOR_PERFORMED: AtomicBool = AtomicBool::new(false);
+
+#[ctor::ctor]
+fn ctor() {
+    assert!(EARLY_CTOR_INITIALIZED.load(Ordering::Relaxed));
+    CTOR_INITIALIZED.store(true, Ordering::Relaxed);
+}
+
+fn main() {
+    assert!(EARLY_CTOR_INITIALIZED.load(Ordering::Relaxed));
+    assert!(CTOR_INITIALIZED.load(Ordering::Relaxed));
+    assert!(MANUAL_CTOR_INITIALIZED.load(Ordering::Relaxed));
+}
+
+#[ctor::dtor]
+fn dtor() {
+    DTOR_PERFORMED.store(true, Ordering::Relaxed);
+}
+
+#[link_section = ".init_array"]
+#[used]
+static INIT_ARRAY: unsafe extern "C" fn(c_int, *mut *mut c_char, *mut *mut c_char) = {
+    unsafe extern "C" fn function(argc: c_int, argv: *mut *mut c_char, envp: *mut *mut c_char) {
+        assert_eq!(argc as usize, std::env::args_os().len());
+
+        assert_eq!(
+            slice::from_raw_parts(argv, argc as usize)
+                .iter()
+                .map(|arg| OsStr::from_bytes(CStr::from_ptr(*arg).to_bytes()).to_owned())
+                .collect::<Vec<OsString>>(),
+            std::env::args_os().collect::<Vec<OsString>>()
+        );
+        assert_eq!(*argv.add(argc as usize), core::ptr::null_mut());
+
+        assert_ne!(envp, core::ptr::null_mut());
+
+        let mut ptr = envp;
+        let mut num_env = 0;
+        loop {
+            let env = *ptr;
+            if env.is_null() {
+                break;
+            }
+
+            let bytes = CStr::from_ptr(env).to_bytes();
+            let mut parts = bytes.splitn(2, |byte| *byte == b'=');
+            let key = parts.next().unwrap();
+            let value = parts.next().unwrap();
+            assert_eq!(
+                std::env::var_os(OsStr::from_bytes(key)).expect("missing environment variable"),
+                OsStr::from_bytes(value)
+            );
+
+            num_env += 1;
+            ptr = ptr.add(1);
+        }
+        assert_eq!(num_env, std::env::vars_os().count());
+
+        MANUAL_CTOR_INITIALIZED.store(true, Ordering::Relaxed);
+    }
+    function
+};
+
+#[link_section = ".init_array.00000"]
+#[used]
+static EARLY_INIT_ARRAY: unsafe extern "C" fn(c_int, *mut *mut c_char, *mut *mut c_char) = {
+    unsafe extern "C" fn function(_argc: c_int, _argv: *mut *mut c_char, _envp: *mut *mut c_char) {
+        EARLY_CTOR_INITIALIZED.store(true, Ordering::Relaxed);
+        assert!(!CTOR_INITIALIZED.load(Ordering::Relaxed));
+        assert!(!MANUAL_CTOR_INITIALIZED.load(Ordering::Relaxed));
+    }
+    function
+};
+
+#[link_section = ".fini_array.00000"]
+#[used]
+static LATE_FINI_ARRAY: unsafe extern "C" fn(c_int, *mut *mut c_char, *mut *mut c_char) = {
+    unsafe extern "C" fn function(_argc: c_int, _argv: *mut *mut c_char, _envp: *mut *mut c_char) {
+        assert!(DTOR_PERFORMED.load(Ordering::Relaxed));
+    }
+    function
+};
---
+++ tests/examples.rs
@@ -0,0 +1,129 @@
+//! Run the programs in the `examples` directory and compare their outputs with
+//! expected outputs.
+
+#![feature(cfg_target_abi)]
+
+extern crate eyra;
+
+use similar_asserts::assert_eq;
+
+macro_rules! assert_eq_str {
+    ($a:expr, $b:expr) => {{
+        assert_eq!(String::from_utf8_lossy($a).lines().collect::<Vec<_>>(), String::from_utf8_lossy($b).lines().collect::<Vec<_>>());
+        assert_eq!($a, $b);
+    }};
+
+    ($a:expr, $b:expr, $($arg:tt)+) => {{
+        assert_eq!(String::from_utf8_lossy($a).lines().collect::<Vec<_>>(), String::from_utf8_lossy($b).lines().collect::<Vec<_>>(), $($arg)+);
+        assert_eq!($a, $b, $($arg)+);
+    }};
+}
+
+fn test_example(name: &str, features: &str, stdout: &str, stderr: &str) {
+    use std::process::Command;
+
+    #[cfg(target_arch = "x86_64")]
+    let arch = "x86_64";
+    #[cfg(target_arch = "aarch64")]
+    let arch = "aarch64";
+    #[cfg(target_arch = "riscv64")]
+    let arch = "riscv64gc";
+    #[cfg(target_arch = "x86")]
+    let arch = "i686";
+    #[cfg(target_arch = "arm")]
+    let arch = "armv5te";
+    #[cfg(target_env = "gnueabi")]
+    let env = "gnueabi";
+    #[cfg(all(target_env = "gnu", target_abi = "eabi"))]
+    let env = "gnueabi";
+    #[cfg(all(target_env = "gnu", not(target_abi = "eabi")))]
+    let env = "gnu";
+
+    let mut command = Command::new("cargo");
+    if which::which("rustup").is_ok() {
+        command.arg("+nightly-2023-09-20");
+    }
+    command.arg("run").arg("--quiet");
+    if !features.is_empty() {
+        command
+            .arg("--no-default-features")
+            .arg("--features")
+            .arg(features);
+    }
+    command
+        .arg(&format!("--target={}-unknown-linux-{}", arch, env))
+        .arg("--example")
+        .arg(name);
+    let output = command.output().unwrap();
+
+    assert_eq_str!(
+        stderr.as_bytes(),
+        &output.stderr,
+        "example {} had unexpected stderr, with {:?}",
+        name,
+        output
+    );
+
+    assert_eq_str!(
+        stdout.as_bytes(),
+        &output.stdout,
+        "example {} had unexpected stdout, with {:?}",
+        name,
+        output
+    );
+    assert!(
+        output.status.success(),
+        "example {} failed with {:?}",
+        name,
+        output
+    );
+
+    // Check nm output for any unexpected undefined symbols.
+    let output = Command::new("nm")
+        .arg("-u")
+        .arg(&format!(
+            "target/{}-unknown-linux-{}/debug/examples/{}",
+            arch, env, name
+        ))
+        .output()
+        .unwrap();
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    // Allow `__rustc_debug_gdb_scripts_section` because it shows up in some
+    // builds and it's a weak external that Rust uses, rather than a reference
+    // to a libc symbol.
+    assert!(
+        stdout == "                 v __rustc_debug_gdb_scripts_section__\n" || stdout == "",
+        "example {} had unexpected undefined symbols:\n{}",
+        name,
+        stdout
+    );
+
+    let output = Command::new("readelf")
+        .arg("-d")
+        .arg(&format!(
+            "target/{}-unknown-linux-{}/debug/examples/{}",
+            arch, env, name
+        ))
+        .output()
+        .unwrap();
+    for line in String::from_utf8_lossy(&output.stdout).lines() {
+        assert!(
+            !line.contains("NEEDED"),
+            "example {} had unexpected dynamic library dependencies: {}",
+            name,
+            line
+        );
+    }
+}
+
+#[test]
+fn test_examples() {
+    test_example("empty", "", "", "");
+    test_example("hello", "", "Hello, world!\n", "");
+    test_example("test-args", "", "", "");
+    test_example("test-ctor", "", "", "");
+    test_example("test-environ", "", "", "");
+    test_example("test-workdir", "", "", "");
+    test_example("test-simd", "", "", "");
+    test_example("test-tls", "", "", "");
+}
