4982aba8158fac1d87c639f612786ff082177445
Dan Gohman <dev@sunfishcode.online>
2023-11-29 22:01:14 -0800

    Minor cleanups.
    Don't use `doc(hidden)`, just tell rustdoc not to generate redundant
    docs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
+#![allow(unused_extern_crates)]
 #![doc = include_str!("../README.md")]
 #![cfg_attr(not(feature = "std"), no_std)]
 
 // If enabled, re-export `std` so that we can be used as `std` to avoid the
 // `extern crate eyra;`.
 #[cfg(feature = "be-std")]
-#[doc(hidden)]
 pub use std::*;
 
 /// All the functionality of Eyra is factored out into separate libraries. This
