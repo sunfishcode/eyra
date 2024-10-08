b903f1bdb0e60c383a3f6ec84ee931dcbc4a79bc
Dan Gohman <dev@sunfishcode.online>
2024-10-08 15:30:28 -0700

    Update to origin 0.21 and c-gull 0.19.

--- example-crates/no-std/src/main.rs
+++ example-crates/no-std/src/main.rs
@@ -1,7 +1,5 @@
 #![no_std]
 #![no_main]
-#![feature(lang_items)]
-#![allow(internal_features)]
 
 extern crate eyra;
 
@@ -9,12 +7,3 @@ extern crate eyra;
 pub extern "C" fn main(_argc: i32, _argv: *const *const u8) -> i32 {
     0
 }
-
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! { loop {} }
-
-#[global_allocator]
-static GLOBAL_ALLOCATOR: rustix_dlmalloc::GlobalDlmalloc = rustix_dlmalloc::GlobalDlmalloc;
-
-#[lang = "eh_personality"]
-extern "C" fn eh_personality() {}
